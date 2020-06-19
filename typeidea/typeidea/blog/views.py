00692f6b953dd2a6f421333aa6da7640dbb0d493
zlj-zz <zlj19971222@outlook.com>
2020-06-19 15:30:35 +0800

    upgrad to `class-based view`

--- typeidea/typeidea/blog/views.py
+++ typeidea/typeidea/blog/views.py
@@ -1,9 +1,86 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from django.shortcuts import get_object_or_404
 
 from .models import Post, Tag, Category
 from config.models import SideBar
 
 
+class CommonViewMixin:
+    """Docstring for CommonViewMixin. """
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_navs())
+
+        return context
+
+
+class IndexView(CommonViewMixin, ListView):
+    """Docstring for PostListView. """
+
+    queryset = Post.latest_posts()
+    paginate_by = 1
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    """Docstring for CategoryView. """
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        # `get_object_or_404`: get object , if not exists return 404
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+
+        return context
+
+    def get_queryset(self):
+
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    """Docstring for TagView. """
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+
+        return context
+
+    def get_queryset(self):
+
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    """Docstring for PostDetailView. """
+
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    comtext_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+
+'''
 def post_list(request, category_id=None, tag_id=None):
     """TODO: Docstring for post_list.
 
@@ -54,3 +131,4 @@ def post_detail(request, post_id=None):
     context.update(Category.get_navs())
 
     return render(request, 'blog/detail.html', context=context)
+'''
